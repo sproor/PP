ba6e5eb00dd635a67a236733d275b357ddb90e61
yehor.pohuliaka <egor86161@gmail.com>
2022-10-19 23:01:36 +0300

    Implemented RemoveMethod

--- PP/Form1.cs
+++ PP/Form1.cs
@@ -19,9 +19,58 @@ namespace PP
             }
         
 
-        public static string RemoveMethod( string InSrc, string InFuncName, string InCode )
+        public static string RemoveMethod( string InUseCode, string InFuncName, string InFuncCode )
         {
-            return InSrc;
+            int             IndexReturn         = InFuncCode.IndexOf("return ");
+            if ( IndexReturn >= 0 )
+            {
+                InFuncCode                      = InFuncCode.Remove( IndexReturn, 7 );       // 6 is size of word 'return '
+                int         IndexSemicolon      = InFuncCode.LastIndexOf(';');
+                if ( IndexSemicolon >= 0 )
+                {
+                    InFuncCode = InFuncCode.Remove( IndexSemicolon, 1 );
+                }
+            }
+
+            string          FunctionSemantic    = InFuncName + "();";
+            if ( InUseCode.Contains( FunctionSemantic ) )
+            {
+                string      CodeFormat = "";
+                int         Index = InUseCode.IndexOf(FunctionSemantic);
+                if ( Index >= 0 )
+                {
+                    string[]    Parts = InUseCode.Split( new[] { FunctionSemantic }, StringSplitOptions.RemoveEmptyEntries );
+                   if ( Parts.Length > 0 && Parts[0].StartsWith(" "))
+                    {
+                        Parts[0] = Parts[0].Remove(0, 1);
+                    }
+                    
+                    if ( Parts.Length > 0 && Index > InUseCode.IndexOf( Parts[0] ) )
+                    {
+                        CodeFormat = Parts[ 0 ] + "{0};";
+                    }
+                    else if ( Parts.Length > 0 )
+                    {
+                        CodeFormat = "{0}; " + Parts[0];
+                    }
+                    else
+                    {
+                        CodeFormat = "{0};";
+                    }
+
+                    for ( int Idx = 1; Idx < Parts.Length; ++Idx )
+                    {
+                        CodeFormat += Parts[ Idx ];
+                    }
+
+                }
+
+                return string.Format( CodeFormat, InFuncCode );
+            }
+            else
+            {
+                return InUseCode;
+            }
         }
 
         public Form1()
@@ -31,7 +80,6 @@ namespace PP
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
         }
     }
 }
